fa123421b6004e8f4344aa6e0ea0257b5d2bed6b
Lorea De La Torre <loreataina@icloud.com>
2025-07-25 19:36:42 -0400

    Add Header component with scroll effect and update styles; include react-icons dependency

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
@@ -20,8 +38,17 @@ export default function Header() {
         </ul>
       </div>
 
-      <div>
-        
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden sm:inline h-6 w-6" />
+        <p className="hidden sm:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="profile icon"
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,22 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "./components/Header";
 
-
 export default function Home() {
   return (
-    <div>
+    <div
+      className="
+    relative 
+    h-screen 
+    bg-gradient-to-b
+    from-gray-900/10
+    to-[#010511]
+    lg:h-[140vh]"
+    >
       <Head>
         <title>Home - Netflix Clone</title>
       </Head>
-      <Header>
-
-      </Header>
+      <Header />
+      <main></main>
     </div>
   );
 }
