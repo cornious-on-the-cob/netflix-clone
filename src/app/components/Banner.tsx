9f348e3bcee41e5f62503ba2e489b12ba109a3f3
Lorea De La Torre <loreataina@icloud.com>
2025-07-25 20:18:11 -0400

    Add Banner component and implement data fetching for movies; update typings and requests

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Banner(netflixOriginals) {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,66 @@
-import Image from "next/image";
-import type { NextPage } from "next";
-import Head from "next/head";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
+import requests from "../../utils/requests";
+import { Movie, Product } from "../../typings";
 
-export default function Home() {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+  products: Product[];
+}
+
+async function getData() {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    netflixOriginals: netflixOriginals.results,
+    trendingNow: trendingNow.results,
+    topRated: topRated.results,
+    actionMovies: actionMovies.results,
+    comedyMovies: comedyMovies.results,
+    horrorMovies: horrorMovies.results,
+    romanceMovies: romanceMovies.results,
+    documentaries: documentaries.results,
+  };
+}
+
+export default async function Home({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+  products,
+}: Props) {
+  const data = await getData();
+
   return (
     <div
       className="
@@ -14,11 +71,10 @@ export default function Home() {
     to-[#010511]
     lg:h-[140vh]"
     >
-      <Head>
-        <title>Home - Netflix Clone</title>
-      </Head>
       <Header />
-      <main></main>
+      <main>
+        <Banner netflixOriginals={netflixOriginals}/>
+      </main>
     </div>
   );
 }
